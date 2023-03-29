03b3c9b2a74c233d52058dab6aa25a745d5b737a
Laura <laura.tracierra@gmail.com>
2023-03-29 20:17:29 -0300

    feat: add update method

--- src/main/java/com/tastecamp/api/controller/CarsController.java
+++ src/main/java/com/tastecamp/api/controller/CarsController.java
@@ -1,12 +1,17 @@
 package com.tastecamp.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,8 +39,28 @@ public class CarsController {
         repository.save(new Car (req));
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Car> update(
+        @PathVariable Long id,
+        @RequestBody @Valid CarDTO req
+    ) {
+        Optional<Car> carData =repository.findById(id);
+
+    if (carData.isPresent()) {
+      Car _car = carData.get();
+      _car.setModelo(req.modelo());
+      _car.setFabricante(req.fabricante());
+      _car.setDataFabricacao(req.dataFabricacao());
+      _car.setValor(req.valor());
+      _car.setAnoModelo(req.anoModelo());
+      return new ResponseEntity<>(repository.save(_car), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    }
+
     @DeleteMapping("/{id}")
-    public void delete (@PathVariable Long id) {
+    public void delete(@PathVariable Long id) {
         repository.deleteById(id);
     }
 }
